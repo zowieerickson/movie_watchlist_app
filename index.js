44b39c4b859df569aa84f6ce0a1e2dc5f59cd6ad
Zowie Erickson <zowie.erickson@gmail.com>
2022-04-09 12:26:24 -0400

    created test html template to be used in JS with real data

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
-console.log('yoyo')
+const searchBtn = document.querySelector("#search-movie-btn")
+const searchText = document.querySelector("#search-movie")
+let searchQuery;
+
+console.log(searchBtn, searchText)
+let html = '';
+
+// fetch(`http://www.omdbapi.com/?apikey=c649a9b9&t=up`)
+//     .then(rsp => rsp.json())
+//     .then(data => {
+//         console.log(data)
+//         html += `<img src=${data.Poster}>`
+//         console.log(html)
+//         document.querySelector("#container-movies").innerHTML = html;
+//     })
+
+searchBtn.addEventListener("click", function() {
+    searchQuery = searchText.value
+    console.log(searchQuery)
+    html = ''
+
+    fetch(`http://www.omdbapi.com/?apikey=c649a9b9&s=${searchQuery}`)
+    .then(rsp => rsp.json())
+    .then(data => {
+        console.log(data)
+        const movies = data.Search;
+        console.log(movies)
+        for(let movie of movies) {
+            html += `
+            <img src=${movie.Poster}>`
+        }
+        document.querySelector("#container-movies").innerHTML = html;
+    })
+})
+
+searchText.addEventListener("keypress", function(e) {
+
+
+    if (e.key === 'Enter') {
+        searchQuery = searchText.value
+        console.log(searchQuery)
+
+        fetch(`http://www.omdbapi.com/?apikey=c649a9b9&t=${searchQuery}`)
+        .then(rsp => rsp.json())
+        .then(data => {
+            console.log(data)
+            html += `<img src=${data.Poster}>`
+            console.log(html)
+            document.querySelector("#container-movies").innerHTML = html;
+        })
+    }
+})

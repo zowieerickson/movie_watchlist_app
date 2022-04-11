ba06ba0f21e26bdef3286e711f32e360f10dbff8
Zowie Erickson <zowie.erickson@gmail.com>
2022-04-11 14:18:58 -0400

    moved html creation into its own function

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ const posterPlaceholder = 'img/poster-placeholder.png';
 let searchQuery;
 let movieId;
 let html = '';
+let readMoreIdArr = [];
+let readMore;
+
+
 
 
 searchBtn.addEventListener("click", function() {
     searchQuery = searchText.value
-    html = ''
 
     fetch(`http://www.omdbapi.com/?apikey=c649a9b9&s=${searchQuery}&page=1&`)
     .then(rsp => rsp.json())
@@ -20,76 +23,83 @@ searchBtn.addEventListener("click", function() {
             fetch(`http://www.omdbapi.com/?apikey=c649a9b9&i=${movieId}&page=1`)
                 .then(rsp => rsp.json())
                 .then(data => {
-                    console.log(data)
-                    console.log(data.Plot)
-                    html += `
-                    <div class="parent-test-flex">
-                        <img class="movie-poster" src="${data.Poster == 'N/A' ? posterPlaceholder : data.Poster}" alt="">
-                        <div class="flex-col">
-                            <div class="second-test-flex"> 
-                                <h2 class="movie-title">${data.Title}</h2>
-                                <img class="movie-star-icon" src="img/star.svg" alt="Yellow star">
-                                <p class="movie-rating">${data.imdbRating}</p>
-                            </div>
-                            <div class="second-test-flex">
-                                <p>${data.Runtime}</p>
-                                <p>${data.Genre}</p>
-                                <p class="movie-add"><a class="flex" href="index.html"><img src="img/plus-add-icon.svg" alt="Addtion icon" class="mrg-rt-sml">Watchlist</a></p>
-                            </div>
-                            <p class="movie-plot">${strEllipsis(data.Plot)}</p>
-                        </div>
-                    </div>`;
+
+                    createMovieHTML(data)
                     moviesContainer.innerHTML = html;
-                    const readMore = document.querySelectorAll(".read-more");
 
-                    console.log(readMore)
+                    // if (data.Plot.length > 230) {
+                    //     readMoreIdArr.push(data.imdbID);
+                    //     console.log(readMoreIdArr)
+                    //     fetch(`http://www.omdbapi.com/?apikey=c649a9b9&i=${movieId}&page=1&plot=full`)
+                    //         .then(rsp => rsp.json())
+                    //         .then(data => console.log)
+                    // }
+                    
+                    // readMore = document.querySelectorAll(".read-more");
+                    // readMore.forEach(element => {
+                    //     element.addEventListener('click', (e) => {
+                    //         fetch(`http://www.omdbapi.com/?apikey=c649a9b9&i=${movieId}&page=1&plot=full`)
+                    //             .then(rsp => rsp.json())
+                    //             .then(data => {
+                    //                 console.log(data);
+                    //                 console.log(data.length)
+                    
+                    //                 document.querySelector(".movie-plot").innerHTML = data.Plot
+                    //             })
+                    //     })
+                    // })
 
-                    readMore.forEach(element => {
-                        element.addEventListener('click', (e) => {
-                            console.log('read more CLICKED');
-                            fetch(`http://www.omdbapi.com/?apikey=c649a9b9&i=${movieId}&page=1&plot=full`)
-                                .then(rsp => rsp.json())
-                                .then(data => {
-                                    // NOT WORKING YET, BUT SOMETHING IS HAPPENING!
-                                    console.log(element)
-                                    console.log(data);
-                                    document.querySelector(".movie-plot").innerHTML = data.Plot
-                                })
-                        })
-                    })
-            })
-        }
+                })
+            }
 
     })
 })
 
-searchText.addEventListener("keypress", function(e) {
-
-
-    if (e.key === 'Enter') {
-        searchQuery = searchText.value
-        console.log(searchQuery)
-
-        fetch(`http://www.omdbapi.com/?apikey=c649a9b9&t=${searchQuery}`)
-        .then(rsp => rsp.json())
-        .then(data => {
-            console.log(data)
-            html += `<img src=${data.Poster}>`
-            console.log(html)
-            document.querySelector("#container-movies").innerHTML = html;
-        })
-    }
-})
-
 function strEllipsis(string) {
     strLength = string.length;
-    if(strLength == 233) {
-        console.log(strLength);
-        console.log('give me dots');
-        console.log(string + '... Read more');
+    if(strLength > 231) {
         return string + '... <a class="read-more"><span class="black-color">Read more</span></a>';
     } else {
         return string;
     }
 }
 
+function createMovieHTML(data) {
+   html += `
+    <div class="parent-test-flex">
+        <img class="movie-poster" src="${data.Poster == 'N/A' ? posterPlaceholder : data.Poster}" alt="">
+        <div class="flex-col">
+            <div class="second-test-flex"> 
+                <h2 class="movie-title">${data.Title}</h2>
+                <img class="movie-star-icon" src="img/star.svg" alt="Yellow star">
+                <p class="movie-rating">${data.imdbRating}</p>
+            </div>
+            <div class="second-test-flex">
+                <p>${data.Runtime}</p>
+                <p>${data.Genre}</p>
+                <p class="movie-add"><a class="flex" href="index.html"><img src="img/plus-add-icon.svg" alt="Addtion icon" class="mrg-rt-sml">Watchlist</a></p>
+            </div>
+            <p class="movie-plot">${strEllipsis(data.Plot)}</p>
+        </div>
+    </div>`;
+
+    return html
+}
+
+
+
+
+
+// Add this later once searchBtn click listener is finished
+// searchText.addEventListener("keypress", function(e) {
+//     if (e.key === 'Enter') {
+//         searchQuery = searchText.value
+
+//         fetch(`http://www.omdbapi.com/?apikey=c649a9b9&t=${searchQuery}`)
+//         .then(rsp => rsp.json())
+//         .then(data => {
+//             html += `<img src=${data.Poster}>`
+//             document.querySelector("#container-movies").innerHTML = html;
+//         })
+//     }
+// })
